2b18a10d02d6d453497696cf80867f07836daefe
Rashmika Siriwardhana <58144396+rnsrashmika078@users.noreply.github.com>
2026-02-02 13:17:09 +0530

    change auth -> firebase -> supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,63 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:gem_store/firebase_options.dart';
-import 'package:gem_store/screens/home_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:superbase_auth/screens/home_screen.dart';
+import 'package:superbase_auth/services/crud.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+  await dotenv.load();
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
-  runApp(Home());
+
+  runApp(MyApp());
+}
+
+final supabase = Supabase.instance.client;
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: AuthApp());
+  }
+}
+
+class AuthApp extends StatefulWidget {
+  const AuthApp({super.key});
+
+  @override
+  State<AuthApp> createState() => _AuthApp();
+}
+
+class _AuthApp extends State<AuthApp> {
+  Map<String, dynamic>? _authUserData;
+  @override
+  void initState() {
+    super.initState();
+    if (!mounted) return;
+
+    supabase.auth.onAuthStateChange.listen((data) async {
+      final user = data.session?.user;
+      final newUserData = data.session?.user.userMetadata;
+      setState(() {
+        _authUserData = newUserData;
+      });
+
+      if (data.event == AuthChangeEvent.signedIn && user != null) {
+        await insertData(user.id);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("SUPABASE + FLUTTER")),
+      body: Center(child: HomeScreen(authUserData: _authUserData)),
+    );
+  }
 }
